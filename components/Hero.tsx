b7397f397c86686806b404f494f2055987e9c7db
PRATANGGA <pratanggajulang31@gmail.com>
2025-07-05 23:40:18 +0700

    Initial Commit

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <Spotlight
+        className="-top-40 -left-10 md:-top-20 md:-left-32 h-screen"
+        fill="white"
+      />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          100: "#000319",
+          DEFAULT: "#000",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
