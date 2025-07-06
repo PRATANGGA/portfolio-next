6bf6869cf6d1ad17baa37322915ce9ce1fef9e57
PRATANGGA <pratanggajulang31@gmail.com>
2025-07-06 14:27:27 +0700

    fix Hero Section add absolute

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
       />
       <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
       <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-      <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
+      <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute">
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
       </div>
 
