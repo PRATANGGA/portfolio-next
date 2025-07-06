cda9f44e5a1cef52f5f681c241bef445b6fa25c8
PRATANGGA <pratanggajulang31@gmail.com>
2025-07-06 10:46:25 +0700

    dev:create hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,8 +11,16 @@ const Hero = () => {
       />
       <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
       <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.3] bg-grid-black/[0.2]">
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+      <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+
+      <div className="flex justify-center items-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Julang Tahta Pratangga
+          </h2>
+        </div>
       </div>
     </div>
   );
